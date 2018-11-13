0fe882d88293a787ccb87e7baf5f890f451971e5
Harlan Evans <harlanevans5@gmail.com>
2018-11-12 17:05:15 -0700

    commit post it notes edit, nav bar, etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'static_notes#home'
+
+  get '/home', to: 'static_notes#home'
+
+  resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
